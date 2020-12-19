3e8120acac714c4e905b30827790d0a30c52c677
snaztoz <hafidhmn@gmail.com>
2020-12-19 14:49:22 +0700

    Menambahkan test untuk mount volume.

--- storage/tests/mount.rs
+++ storage/tests/mount.rs
@@ -1,6 +1,21 @@
 use storage::{ErrorKind, Storage};
 
+use serial_test::serial;
+
 mod util;
+
+#[test]
+#[serial]
+fn mount_valid_volume() {
+    assert!({
+        util::create_volume();
+
+        let mut s = Storage::new();
+        let res = s.mount(path_of!("tmp/test.neondb"));
+
+        res.is_ok()
+    });
+}
 
 #[test]
 fn mount_non_existing_volume() {
--- storage/tests/util/mod.rs
+++ storage/tests/util/mod.rs
@@ -1,3 +1,7 @@
+use storage::NEONDB_FILE_SIZE;
+
+use std::fs::File;
+
 #[macro_export]
 macro_rules! path_of {
     ($p: expr) => {{
@@ -6,4 +10,9 @@ macro_rules! path_of {
         let path = concat!("../", $p);
         Path::new(path)
     }};
+}
+
+pub fn create_volume() {
+    let vol = File::create(path_of!("tmp/test.neondb")).unwrap();
+    vol.set_len(NEONDB_FILE_SIZE).unwrap();
 }
