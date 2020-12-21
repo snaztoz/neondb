abbf33646bce0fc31049fb472070e01fcac853c7
snaztoz <hafidhmn@gmail.com>
2020-12-21 22:42:30 +0700

    Membuat basic test untuk Storage::aloc.

---
+++ storage/tests/alloc.rs
@@ -0,0 +1,37 @@
+use storage::{ErrorKind, Storage};
+
+use serial_test::serial;
+
+mod util;
+
+#[test]
+#[serial]
+fn alloc_block() {
+    assert!({
+        let mut s = Storage::new();
+        s.mount(path_of!("tmp/test.neondb"))
+            .expect("mounting test volume");
+
+        let res = s.alloc(20); // alokasi 20 byte
+
+        res.is_ok()
+    });
+}
+
+#[test]
+#[serial]
+fn alloc_exceeding_volume_cap() {
+    assert!({
+        let mut s = Storage::new();
+        s.mount(path_of!("tmp/test.neondb"))
+            .expect("mounting test volume");
+
+        let res = s.alloc(1 << 32);
+
+        if let Err(ErrorKind::VolumeNotEnoughSpace) = res {
+            true
+        } else {
+            false
+        }
+    });
+}
