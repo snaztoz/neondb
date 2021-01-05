56c5d5fd3655b6d8aac983084c7b61d48f6cd966
snaztoz <hafidhmn@gmail.com>
2021-01-05 19:16:46 +0700

    Refactor test.

--- storage/tests/util/mod.rs
+++ storage/tests/util/mod.rs
@@ -6,10 +6,8 @@ use std::io::{prelude::*, SeekFrom};
 #[macro_export]
 macro_rules! path_of {
     ($p: expr) => {{
-        use std::path::Path;
-
         let path = concat!("../", $p);
-        Path::new(path)
+        std::path::Path::new(path)
     }};
 }
 
