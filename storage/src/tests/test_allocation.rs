eac181eeef4d5ec1fc6cafbc797107b1148c0104
snaztoz <hafidhmn@gmail.com>
2021-01-06 20:54:47 +0700

    Menambahkan test untuk alokasi blok.

--- storage/src/tests/test_allocation.rs
+++ storage/src/tests/test_allocation.rs
@@ -24,3 +24,22 @@ fn alloc_one_block() {
         blocks[0].address == address && blocks[0].size == 64
     });
 }
+
+#[test]
+#[serial]
+fn alloc_blocks() {
+    assert!({
+        let mut s = init_storage();
+
+        for i in 0..3 {
+            let address = s.alloc(64).unwrap();
+            let block = &s.blocks().unwrap()[i];
+
+            if block.address != address || block.size != 64 {
+                panic!("wrong address/size on allocation");
+            }
+        }
+
+        true
+    });
+}
