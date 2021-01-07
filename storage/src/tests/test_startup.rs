df3cc366dd8e2e4f3c56083361fb16bc1efd85bc
snaztoz <hafidhmn@gmail.com>
2021-01-07 07:39:24 +0700

    Menambahkan test untuk startup.

--- storage/src/tests/test_startup.rs
+++ storage/src/tests/test_startup.rs
@@ -42,3 +42,44 @@ fn startup_with_blocks_exist() {
         s.blocks().unwrap().last().unwrap().address == address
     });
 }
+
+#[test]
+#[serial]
+fn startup_after_dealloc_blocks() {
+    let blocks = {
+        let mut s = init_storage();
+        let mut dealloc_addresses = vec![];
+
+        for i in 0..10 {
+            let address = s.alloc(64).unwrap();
+            if i == 0 || i == 9 {
+                continue;
+            }
+            dealloc_addresses.push(address);
+        }
+
+        // buat gap di antara blok awal dengan akhir
+        for address in dealloc_addresses {
+            s.dealloc(address).unwrap();
+        }
+
+        s.blocks().unwrap()
+    }; // drop s terlebih dulu
+
+    assert!({
+        let mut s = Storage::new();
+        s.mount(path_of!("tmp/storage/startup.neondb")).unwrap();
+
+        blocks == s.blocks().unwrap()
+    });
+
+    // Coba alokasi kembali setelah startup
+    assert!({
+        let mut s = Storage::new();
+        s.mount(path_of!("tmp/storage/startup.neondb")).unwrap();
+
+        let address = s.alloc(64 * 9).unwrap();
+
+        s.blocks().unwrap()[1].address == address
+    });
+}
