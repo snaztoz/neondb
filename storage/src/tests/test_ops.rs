923f8ef44baa524c144023235f999aaf8c170619
snaztoz <hafidhmn@gmail.com>
2021-01-07 20:54:18 +0700

    Menambahkan test untuk operasi storage.

--- storage/src/tests/test_ops.rs
+++ storage/src/tests/test_ops.rs
@@ -3,7 +3,7 @@ use crate::{ErrorKind, Storage};
 
 use serial_test::serial;
 
-use std::fs::File;
+use std::fs::OpenOptions;
 use std::io::{prelude::*, SeekFrom};
 
 fn init_storage() -> Storage {
@@ -13,6 +13,18 @@ fn init_storage() -> Storage {
     s.mount(path_of!("tmp/storage/ops.neondb")).unwrap();
 
     s
+}
+
+fn write_ones(address: u64, size: usize) {
+    let mut vol = OpenOptions::new()
+        .read(true)
+        .write(true)
+        .open(path_of!("tmp/storage/ops.neondb"))
+        .unwrap();
+
+    vol.seek(SeekFrom::Start(address))
+        .and_then(|_| vol.write(&vec![1u8; size]))
+        .unwrap();
 }
 
 #[test]
@@ -37,11 +49,7 @@ fn read_block_bytes() {
         true
     });
 
-    // coba tulis beberapa byte
-    let mut vol = File::create(path_of!("tmp/storage/ops.neondb")).unwrap();
-    vol.seek(SeekFrom::Start(address))
-        .and_then(|_| vol.write(&[1u8; 16]))
-        .unwrap();
+    write_ones(address, 16);
 
     assert!({
         let res = s.read(address, &mut buff);
@@ -65,5 +73,39 @@ fn read_at_illegal_address() {
         let res = s.read(234653, &mut buff);
 
         matches!(res, Err(ErrorKind::BlockNotFound))
+    });
+
+    assert!({
+        let mut s = init_storage();
+        let mut buff = [0u8; 1];
+
+        let address = s.alloc(64).unwrap();
+
+        let res = s.read(address + 64, &mut buff);
+
+        res.is_err()
+    });
+}
+
+#[test]
+#[serial]
+fn read_truncated() {
+    assert!({
+        let mut s = init_storage();
+        let mut buff = [0u8; 64];
+
+        let address = s.alloc(64).unwrap();
+        write_ones(address, 64);
+
+        // membaca mulai dari posisi tengah blok
+        let res = s.read(address + 32, &mut buff);
+
+        match res {
+            Ok(n) => assert!(n == 32),
+            Err(_) => panic!("reading failed"),
+        }
+
+        // 1 sebanyak 32 kali, lalu 0 sebanyak 32 kali
+        buff[..32] == [1u8; 32] && buff[32..] == [0u8; 32]
     });
 }
