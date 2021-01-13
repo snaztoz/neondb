1cc631988075d6f097059731db4cb581c49f4343
snaztoz <hafidhmn@gmail.com>
2021-01-13 22:35:52 +0700

    Membuat sebuah test.

--- parser/src/tests/test_lexer.rs
+++ parser/src/tests/test_lexer.rs
@@ -1,1 +1,20 @@
+use crate::lexer::Lexer;
+use crate::token::Token;
 
+#[test]
+fn lex_query() {
+    assert!({
+        let query = "SELECT * FROM my_table;";
+        let mut lexer = Lexer::new(query.chars());
+        let tokens = lexer.lex().unwrap();
+
+        tokens
+            == vec![
+                Token::Select,
+                Token::Asterisk,
+                Token::From,
+                Token::Name(String::from("my_table")),
+                Token::Semicolon,
+            ]
+    });
+}
